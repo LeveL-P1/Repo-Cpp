0686a9ced849f4342b167506b24675e3976f3671
Level-P1 <pawanbanait.dev@gmail.com>
2026-02-28 22:36:56 +0530

    class 2: ifelse and for loop

---
+++ C++/ifelse.cpp
@@ -0,0 +1,59 @@
+
+/*
+Problem 1 — Easy
+Take a number as input. Print "Positive", "Negative", or "Zero" based on the number.
+*/
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>> a;
+//     cout << ( -1>a<+1  ? "Positve": "Negative");
+//     return 0;
+// }
+
+
+// Problem 2 — Medium
+// Take two numbers as input. Print whichever is bigger. If both are equal print "Equal".
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     int b;
+//     cout<<"enter a :";
+//     cin>> a ;
+//     cout<< "enter b:";
+//     cin >>  b;
+   
+//     if (a>b ){
+//         cout<<"a is bigger"<<endl;
+//     } else if (b>a){
+//         cout<<"b is bigger"<< endl;
+//     }else{
+
+//         cout<< "both are equal";
+//     }
+//     // cout<< "both are equal\n";
+//     return 0;
+// }
+
+
+// problem 3
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int year;
+//     cin >> year;
+
+//     if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+//         cout << "Leap Year";
+//     } else {
+//         cout << "Not Leap Year";
+//     }
+
+//     return 0;
+// }
---
+++ C++/loops.cpp
@@ -0,0 +1,51 @@
+
+
+// problem 1
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     for (int i = 2; i <= 50; i += 2) {
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
+
+
+// Problem 2 — Medium
+// Take a number n as input. Print its factorial (factorial means: n × n-1 × n-2 ... × 1. Example: factorial of 5 = 5×4×3×2×1 = 12
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {   
+//     int res= 1;
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//      res *= i ;
+//     }
+//     cout << res;
+
+//     return 0;
+// }
+
+// Problem 3 — Hard
+// Take a number n as input. Print this pattern 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//      int n;
+//     cin >> n;
+
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= i; j++) {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
