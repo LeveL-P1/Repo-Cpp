f303f78dfbbffb55d26b2a5aeaee1b7540e62148
Level-P1 <pawanbanait.dev@gmail.com>
2026-03-01 21:57:20 +0530

    class 3: basic function

---
+++ C++/func.cpp
@@ -0,0 +1,72 @@
+// Function to print square of a number
+
+// Function to check even or odd (return bool)
+
+// Function to find maximum of two numbers
+
+// Function to calculate factorial using function
+
+
+// 1
+
+// #include<iostream>
+// using namespace std;
+
+// int add(int a){
+//     return a*a;
+
+// }
+
+// int main(){
+//     int res= add(2);
+//     cout <<res;
+// }
+
+
+// 2
+
+// #include <iostream>
+// using namespace std;
+
+// string  evnodd(int a)
+// {
+
+//     if (a % 2 == 0)
+//     {
+//         return "even";
+//     }
+//     else
+//     {
+//         return "odd";
+//     }
+// }
+
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     cout << evnodd(a) ;
+//     return 0;
+// }
+
+
+// 3
+
+// #include<iostream>
+// using namespace std;
+
+// string  max(int a, int b){
+//     if (a>b){
+//         return "a is max";
+//     }else{
+//         return "b is max";
+//     }
+// }
+
+// int main(){
+//     int x, y;
+//     cin >> x>>y;
+//     cout << max(x,y);
+//     return 0;
+// }
+
